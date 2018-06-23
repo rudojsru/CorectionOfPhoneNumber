956b0a9dc517e1ad1a02eec8e1eda178c992cfff
Olexandr Rudyy <rudojsru@gmail.com>
2018-06-23 19:23:48 +0200

    3

---
+++ src/PracaZPlikem.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+        import java.io.FileNotFoundException;
+        import java.io.FileReader;
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.List;
+public class PracaZPlikem {
+    public static final String TEXT="Text.txt";
+    private BufferedReader br;
+
+    public PracaZPlikem(){
+    }
+
+    public void openFile(){
+        try{
+            br=new BufferedReader(new FileReader(TEXT));
+            System.out.println("Fileisopen");
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String wczytaj(){
+        if(br==null){
+//jeslizamkniety
+            openFile();
+        }
+        try{
+            String linia=br.readLine();
+            if(linia==null){
+                przevinPlik();
+                linia=br.readLine();
+            }
+            return linia;
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void przevinPlik(){
+        closeFile();
+        openFile();
+    }
+
+    public void closeFile(){
+        System.out.println("file is clouse");
+        try{
+            br.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void pominLinije(){
+        System.out.println("Pominlinije");
+        wczytaj();
+
+    }
+
+    public void pominNLinij(int n){
+        System.out.println("Pomineliem"+n+"linij");
+        for(int i=0;i<n;i++){
+            wczytaj();
+        }
+    }
+
+    public List<String>wczytajNLinij(int n){
+        List<String>wczytaj=new ArrayList<String>();
+        for(int i=0;i<n;i++){
+            wczytaj.add(wczytaj());
+
+        }
+        return wczytaj;
+    }
+
+
+}
---
+++ src/WorkWithFile.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+// Olexandr Rudyy 23.06.2018
+// rudojs@gmail.com
+
+public class WorkWithFile {
+    public static final String TEXT ="src/originText.txt";
+    public static final String TEXTmodified ="src/modifiedText.txt";
+    private static BufferedReader br;
+    private static List<String> lines;
+
+
+
+    public static void main(String[] args) {
+        readFile();
+       // writerFile();
+    }
+
+   private static void readFile(){
+        List<String> lines =new LinkedList<>();
+        String line=null;
+        try {
+            br = new BufferedReader(new FileReader(TEXT));
+
+            while ((line=br.readLine())!= null) {
+
+                lines.add(line);
+            }
+            br.close();
+            System.out.println("Input file: "+lines);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static  void writerFile(){
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(TEXTmodified);
+            BufferedWriter bw=new BufferedWriter(fileWriter);
+
+            for(int i =lines.size()-1; i>0; i-- ){
+                bw.write(lines.get(i)+";"+"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
